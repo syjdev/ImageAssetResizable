27ae42d60ff30b4a855179c483c9563df6470aa8
syjdev <syjdev@gmail.com>
2023-06-26 01:53:01 +0900

    1) development, production 각각에 따라, webpack 설정 일부를 분기처리 하였음. 2) script 수정.

--- webpack.config.js
+++ webpack.config.js
@@ -5,71 +5,69 @@ const HtmlPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
-// const NODE_ENV = process.env.NODE_ENV || "development";
-const isProd = false;
-
-console.log("#### - " + __dirname);
-
 const publicPath = resolve(__dirname, "./public");
 const outputPath = resolve(__dirname, "./dist");
 
-module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  entry: resolve(__dirname, "./src/main.ts"),
-  output: {
-    filename: "js/[name].[contenthash].js",
-    chunkFilename: "js/[name].[contenthash].js",
-    path: outputPath,
-    // clean: true,
-  },
+module.exports = (env, argv) => {
+  const isDevelopmentPhase = argv.mode === "development";
 
-  resolve: {
-    extensions: [".ts", ".js", ".vue", ".json"],
-    alias: {
-      "~": path.resolve(__dirname, "src"),
-      assets: path.resolve(__dirname, "src/assets"),
+  return {
+    mode: isDevelopmentPhase ? "development" : "production",
+    devtool: isDevelopmentPhase ? "source-map" : false,
+    entry: resolve(__dirname, "./src/main.ts"),
+    output: {
+      filename: "js/[name].[contenthash].js",
+      chunkFilename: "js/[name].[contenthash].js",
+      path: outputPath,
     },
-  },
 
-  devServer: {
-    port: 8080,
-    hot: true,
-  },
-
-  stats: "errors-warnings",
-
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: "vue-loader",
+    resolve: {
+      extensions: [".ts", ".js", ".vue", ".json"],
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+        assets: path.resolve(__dirname, "src/assets"),
       },
-      {
-        test: /\.(css|scss|sass)$/i,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
+    },
+
+    devServer: {
+      port: 8080,
+      hot: true,
+    },
+
+    stats: "errors-warnings",
+
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: "vue-loader",
         },
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: "file-loader",
-      },
-    ],
-  },
+        {
+          test: /\.(css|scss|sass)$/i,
+          use: ["vue-style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: "file-loader",
+        },
+      ],
+    },
 
-  plugins: [
-    new HtmlPlugin({
-      template: resolve(__dirname, "./index.html"),
-    }),
-    new CopyPlugin({
-      patterns: [{ from: publicPath, to: outputPath }],
-    }),
-    new VueLoaderPlugin(),
-  ],
+    plugins: [
+      new HtmlPlugin({
+        template: resolve(__dirname, "./index.html"),
+      }),
+      new CopyPlugin({
+        patterns: [{ from: publicPath, to: outputPath }],
+      }),
+      new VueLoaderPlugin(),
+    ],
+  };
 };
